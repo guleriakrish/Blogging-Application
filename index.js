4bf2c15fe11afd851d0b403166d06d6a80915f00
guleriakrish <krishguleria79@gmail.com>
2026-04-07 21:09:30 +0530

    changes in index.js after niew view creation

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ const port=2233;
 app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
 
+app.get("/",(req,res)=>{
+	res.render("home");
+});
 app.listen(port,console.log(`[SERVER STARTED AT PORT ${port}]`));
