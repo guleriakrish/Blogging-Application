e1e4be345fe26f386a2f80e6706ffb776ad58887
guleriakrish <krishguleria79@gmail.com>
2026-04-12 23:09:06 +0530

    jsonwebtokens library installed and made changes to connnection file

--- connection/index.js
+++ connection/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export default async function c(url){
-	await mongoose.connect(url);
-	console.log("[MONGOOSE CONNECTED]");
+	await mongoose.connect(url).then(()=>{;
+	console.log("[MONGOOSE CONNECTED]");}).catch(()=>{
+		console.log("[MONGOOSE NOT CONNECTED]");
+	});
 }
