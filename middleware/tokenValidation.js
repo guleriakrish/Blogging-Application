67dfb0ec31a839ef05a5baf5d3412112ece2e849
guleriakrish <krishguleria79@gmail.com>
2026-04-14 14:30:39 +0530

    navbar done according to login

--- middleware/tokenValidation.js
+++ middleware/tokenValidation.js
@@ -5,15 +5,16 @@ import authentication from "../services/authentication.js";
 export default function c(tokenName){
 	return (req,res,next)=>{
 		if(req.url=="/favicon.ico") return res.status(204).end();
-		console.log("Request URL:", req.url, "\n");
+//		console.log("Request URL:", req.url, "\n");
 		const tokenValue=req.cookies[tokenName];
-		console.log(`tokenCookieValue: ${tokenValue}`);  
+//		console.log(`tokenCookieValue: ${tokenValue}`);  
 		if(tokenValue){
 			try{
 //				console.log("inside mware213");
 				const userPayload=authentication.validateToken(tokenValue);
 //				console.log("\n AFTER USERPAYLOAD \n");
 //				console.log(userPayload);
+				req.user=userPayload;
 			}catch{}
 
 		}
--- services/authentication.js
+++ services/authentication.js
@@ -6,6 +6,7 @@ function createTokenForUser(user){
 	const payload={
 		_id:user._id,
 		email:user.email,
+		fullname:user.fullname,
 		profileImageURL: user.profileImageURL,
 		role:user.role,
 	};
