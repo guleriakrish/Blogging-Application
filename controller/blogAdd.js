07f9034167e20c752804a356706e1b4726559e65
guleriakrish <krishguleria79@gmail.com>
2026-04-22 09:42:36 +0530

    blog uploading done

--- controller/blogAdd.js
+++ controller/blogAdd.js
@@ -15,15 +15,14 @@ function get(user,blog){
 function post(user,blog){
     return async function(req,res){
         console.log(req.body);
-        if(req.user){
-            const user=req.user;
-            res.render("blogAdd",{
-                user,
-            });
-        }
-        else{
-            res.render("blogAdd");
-        }
+        console.log(req.file);
+        const bg=await blog.create({
+            title:req.body.titleOfBlog,
+            body:req.body.contentOfBlog,
+            coverImageUrl:`./public/uploads/${req.file.filename}`,
+            createdBy:req.user._id
+        });
+        return res.redirect(`/blog/${bg._id}`);
     }
 }
 
--- routes/blog.js
+++ routes/blog.js
@@ -2,9 +2,31 @@ import express from "express";
 import blogAdd from "../controller/blogAdd.js";
 import user from "../model/userModel.js"
 import blog from "../model/blog.js";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        const pathToFile=path.resolve(`./public/uploads/${req.user._id}`);
+        
+        if(!fs.existsSync(pathToFile)){
+            fs.mkdirSync(pathToFile,{recursive:true});
+        }
+
+        cb(null,pathToFile);
+    },
+    filename:function(req,file,cb){
+        const filename=`${Date.now()}-${file.originalname}`;
+        req.filename=filename;
+        cb(null,filename);
+    }
+})
+
+const upload=multer({storage:storage});
 
 const router=express.Router();
 
 router.get("/add",blogAdd.get(user,blog));
-router.post("/add",blogAdd.post(user,blog));
+router.post("/add",upload.single('coverOfBlog'),blogAdd.post(user,blog));
 export default router;  
